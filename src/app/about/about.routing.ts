e45fa21400b9b97470eaea6bcca3798ab4b0c7ec
Rob-Rychs <Robrychel@gmail.com>
2017-08-29 09:48:45 -0700

    added child routing for about component

--- src/app/about/about.routing.ts
+++ src/app/about/about.routing.ts
@@ -2,10 +2,26 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ModuleWithProviders } from '@angular/core';import { AboutComponent } from  './about.component';
 import { AboutUserComponent } from  '../about-user/about-user.component';
+import { AboutSectionComponent } from './about-section.component';
 
 const aboutRoutes: Routes = [
-  { path: 'about', component: AboutComponent },
-  { path: 'about/:username', component: AboutUserComponent },
+  // child routing
+  { path: 'about',
+  component: AboutSectionComponent,
+    children: [
+      {
+        path: '',
+        component: AboutComponent
+      },
+      {
+        path: ':username',
+        component: AboutUserComponent
+      }
+    ]
+  }
+  // examples of standard routing  
+  // { path: 'about', component: AboutComponent },
+  // { path: 'about/:username', component: AboutUserComponent },
 ];
 
 export const aboutRouting: ModuleWithProviders = RouterModule.forChild(aboutRoutes)
