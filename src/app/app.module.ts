f2f26156f86c1b6a21e6b713801ebcd97ec40f14
Rob-Rychs <Robrychel@gmail.com>
2017-08-29 09:23:31 -0700

    refactor about components into aboutModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AboutModule } from './about/about.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
+
 import { appRouting } from './app.routing';
 import { NotfoundComponent } from './notfound/notfound.component';
-import { AboutUserComponent } from './about-user/about-user.component';
-import { UserService } from './shared/services/user.service';
+
+
+import { FileSelectDirective, FileDropDirective, FileUploader } from 'ng2-file-upload/ng2-file-upload';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactComponent,
-    AboutComponent,
-    NotfoundComponent,
-    AboutUserComponent
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    appRouting
+    appRouting,
+    AboutModule
   ],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,20 +2,18 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
 import { NotfoundComponent } from './notfound/notfound.component';
-import { AboutUserComponent } from './about-user/about-user.component';
+
 
 const appRoutes: Routes = [
-  // redirect home page to about page
-  {
-    path: '',
-    redirectTo: '/about',
-    pathMatch: 'full'
-  },
-  // { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'about/:username', component: AboutUserComponent },
+  // commentee out redirect home page to about page
+  // {
+  //   path: '',
+  //   redirectTo: '/about',
+  //   pathMatch: 'full'
+  // },
+  { path: '', component: HomeComponent },
+ 
   { path: 'contact', component: ContactComponent },
   { path: '**', component: NotfoundComponent }
 ];
